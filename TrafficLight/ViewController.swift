b62de7bb33736e42f59cc60d2e58e2ebebef4725
Ewgeny Izyurov <help@izyapp.ru>
2024-09-25 20:00:11 +0500

    created connections between storyboard and ViewController

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var light: UIView!
+    
+    @IBOutlet var startButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
 
-
+    @IBAction func buttonDidPressed(_ sender: UIButton) {
+    }
+    
 }
 
