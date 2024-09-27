6d576d6a8e61da40c3102a49329885ecda611065
Ewgeny Izyurov <help@izyapp.ru>
2024-09-28 01:48:42 +0500

    Assigned an action to the button, added rounding to the objects.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,17 +7,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet var light: UIView!
+final class ViewController: UIViewController {
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowButton: UIView!
+    @IBOutlet var greenLight: UIView!
     
     @IBOutlet var startButton: UIButton!
+    
+    private var counterForPressed = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        makeBeautifulInterface()
     }
 
+    private func makeBeautifulInterface(){
+        startButton.layer.cornerRadius = 10
+        
+        let views = [redLight, yellowButton, greenLight]
+        
+        for view in views {
+            view?.layer.cornerRadius = (view?.frame.width ?? 0) / 2
+        }
+    }
+    
     @IBAction func buttonDidPressed(_ sender: UIButton) {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        counterForPressed += 1
+        
+        switch counterForPressed {
+        case 0:
+            redLight.alpha = 1
+            greenLight.alpha = 0.3
+        case 1:
+            redLight.alpha = 0.3
+            yellowButton.alpha = 1
+        case 2:
+            yellowButton.alpha = 0.3
+            greenLight.alpha = 1
+            counterForPressed = -1
+        default:
+            print("Error")
+        }
     }
     
 }
